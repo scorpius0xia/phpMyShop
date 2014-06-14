c3878bc5c20e5d70e9ae379524f7e6bb4a8589d7
coolwind <ht20092009@126.com>
2014-06-14 09:28:15 +0800

    完成数据库设计

--- sql_source/phpMyShop.sql
+++ sql_source/phpMyShop.sql
@@ -1,14 +1,37 @@
-create table users(
-uid int,
+create table if not exists users(
+uid int primary key auto_increment,
 uname varchar(20) unique,
 upassword varchar(50),
 uemail varchar(320),
-admin tinyint,
-primary key (uid)
+admin tinyint
 );
 
-create table goods(
+create table if not exists good(
+gid int primary key auto_increment,
+gname varchar(20)
+);
+
+create table if not exists user_detail(
+udid int primary key auto_increment,
+uname varchar(20),
+udaddress text,
+udtelephone varchar(13),
+foreign key(uname) references users(uname)
+);
+
+create table if not exists good_detail(
+gdid int primary key auto_increment,
 gid int,
-gname varchar(20),
-primary key(gid)
+gdprice int,
+gdamount int,
+foreign key(gid) references good(gid)
+);
+
+create table if not exists orders(
+oid int primary key auto_increment,
+uid int,
+oname int,
+oaddress text,
+otelephone varchar(13),
+foreign key (uid) references users(uid)
 );
